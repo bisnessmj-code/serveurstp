65a89175e7bd7946e74cc97444354c4c915808d5
bisnessmj-code <bisness.mj@gmail.com>
2026-02-05 15:15:53 +0100

    save

---
+++ area_lobby_dogan4/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+game 'gta5'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+Author 'Baasha Bhai'
+
+--file "sp_manifest.ymt"
+--data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
+
+
+-- replace_level_meta 'gta5'
+
+-- files {
+-- 	'gta5.meta',
+-- }
+-- dependency '/assetpacks'
+
+
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/increasedprops_interiors.ytyp'
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/increasedprops_terrain.ytyp'
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/v_minigame.ytyp'
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/v_industrial_2.ytyp'
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/increasedprops_props.ytyp'
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/builderdefaddons.ytyp'
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/ark_mansion.ytyp'
+
+-- data_file 'DLC_ITYP_REQUEST' 'stream/arkv_water.ytyp'
---
+++ cfx_podiumV2/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'EcX_Diablo'
+description 'Cfx_Podium'
+version '1.0.2'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/rcnk_podium.ytyp',
+    'stream/podium.ymap'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/rcnk_podium.ytyp'
+data_file 'MAP' 'stream/podium.ymap'
